8a99655c94885ce4a9927e955d9ff0f7a81c0b10
evg <evg-desktop@gmail.com>
2024-07-31 11:02:44 +0600

    added templating

---
+++ django_blog/article/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+# from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    title = 'Эта странница статей блога'
+    return render(
+        request,
+        'articles/index.html',
+        context={'title': title}
+        )
